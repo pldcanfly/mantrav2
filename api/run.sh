8f3b91a4b9381ec96d23c6542862c67cf295a74b
pldcanfly <pldcanfly@gmail.com>
2022-09-25 03:42:26 +0200

    Adjusted build

--- api/run.sh
+++ api/run.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-if [[ $# -ne 2 ]] ; then
-    echo 'Usage: /build.sh <tag> <repo>'
-    exit 0
-fi
 
-
-docker stop $1
-docker rm $1
-docker run -itd -p 4000:4000 --name $1 $2/$1
-docker start $1
+docker stop mantra
+docker rm mantra
+docker run -itd -p 4000:4000 --name mantra pldcanfly/mantra
+docker start mantra
 docker ps
